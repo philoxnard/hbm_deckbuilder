dd8ac0edee5e5b0f42da8632ddea4afa22de391b
Phil <phil.oxnard@gmail.com>
2024-08-01 09:29:05 -0400

    fixed some outdated card text

--- messageHandlers.py
+++ messageHandlers.py
@@ -21,7 +21,7 @@ def handleOnLoad(message):
 
     response["cards"] = cards
     response["filters"] = filters
-    response["version"] = "2.1.0"
+    response["version"] = "2.1.1"
     response["result"] = "Success"
 
     return response
