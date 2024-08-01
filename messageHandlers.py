4d7a000e8cb20eb2d8b8d271c219ba06e2aa962c
Phil <phil.oxnard@gmail.com>
2024-08-01 09:14:45 -0400

    Fixed old crew names

--- messageHandlers.py
+++ messageHandlers.py
@@ -21,7 +21,7 @@ def handleOnLoad(message):
 
     response["cards"] = cards
     response["filters"] = filters
-    response["version"] = "2.0.8"
+    response["version"] = "2.0.9"
     response["result"] = "Success"
 
     return response
--- static/javascript.js
+++ static/javascript.js
@@ -489,7 +489,7 @@ function drawFilters(filters) {
     $("#filter_modal_column_2").append("<input type='text' id='firepower_filter' name='firepower_filter'><br><br>")
 
     $("#filter_modal_column_2").append('Input crew to display:<br>')
-    $("#filter_modal_column_2").append("<select name='crew_filter' id='crew_filter'><option value='None'>None</option><option value='starter'>Starter</option><option value='bulwark'>Bulwark</option><option value='reef'>Reef</option><option value='sand'>Sand</option></select>")
+    $("#filter_modal_column_2").append("<select name='crew_filter' id='crew_filter'><option value='None'>None</option><option value='starter'>Starter</option><option value='fearless'>Fearless</option><option value='clever'>Clever</option><option value='mystical'>Mystical</option></select>")
 
     $("#filter_modal_column_2").append("<button id='filter_button' onclick='submitFilters()'>Filter Cards</button>")
 
