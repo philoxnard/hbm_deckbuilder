dfcb741cda52fa3db8778cc1e4ad14c07f778849
Phil <phil.oxnard@gmail.com>
2024-08-29 10:10:14 -0400

    fix gunneliel from heavy metal shanty rock

--- messageHandlers.py
+++ messageHandlers.py
@@ -21,7 +21,7 @@ def handleOnLoad(message):
 
     response["cards"] = cards
     response["filters"] = filters
-    response["version"] = "2.1.6"
+    response["version"] = "2.1.7"
     response["result"] = "Success"
 
     return response
