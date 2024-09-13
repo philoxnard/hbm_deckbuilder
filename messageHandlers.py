f86a7e4d65ae68c18cd945c56dc66dd28f51dd46
Phil <phil.oxnard@gmail.com>
2024-09-13 09:32:25 -0700

    fix anenomoe silver card count

--- messageHandlers.py
+++ messageHandlers.py
@@ -21,7 +21,7 @@ def handleOnLoad(message):
 
     response["cards"] = cards
     response["filters"] = filters
-    response["version"] = "2.2.0"
+    response["version"] = "2.2.1"
     response["result"] = "Success"
 
     return response
