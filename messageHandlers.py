8c417232450e1ac28e2243c295ccd8312ddb5e64
Phil <phil.oxnard@gmail.com>
2024-09-16 10:18:16 -0400

    updated lots of cards

--- messageHandlers.py
+++ messageHandlers.py
@@ -21,7 +21,7 @@ def handleOnLoad(message):
 
     response["cards"] = cards
     response["filters"] = filters
-    response["version"] = "2.2.1"
+    response["version"] = "2.2.2"
     response["result"] = "Success"
 
     return response
