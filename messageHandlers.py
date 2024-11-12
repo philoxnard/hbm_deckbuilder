c5479c25f83cd3c59eed2d3116f6d2b40ca3485d
Phil <phil.oxnard@gmail.com>
2024-11-12 11:38:53 -0500

    no doombeard trio, added image for ship rex

--- messageHandlers.py
+++ messageHandlers.py
@@ -21,7 +21,7 @@ def handleOnLoad(message):
 
     response["cards"] = cards
     response["filters"] = filters
-    response["version"] = "2.2.3"
+    response["version"] = "2.2.4"
     response["result"] = "Success"
 
     return response
